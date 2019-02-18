7c965741ca40698fd50d254884d6cab7bcaff942
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2019-02-18 12:26:44 +0900

    update test

--- c_impl/tests/value_test.c
+++ c_impl/tests/value_test.c
@@ -34,7 +34,58 @@ TEST_CASE(test_array, {
   for (int i = 0; i < 10; i++) {
     assert(tv_getLong(array->data[i]) == i);
   }
+
+  array = new_vec();
+  size_t strs_len = 3;
+  sds *strs = xmalloc(sizeof(sds) * strs_len);
+  strs[0] = sdsnew("abc");
+  strs[1] = sdsnew("def");
+  strs[2] = sdsnew("ghi");
+  for (size_t i = 0; i < strs_len; i++) {
+    vec_push(array, new_TValue_with_str(strs[i]));
+  }
+  for (size_t i = 0; i < strs_len; i++) {
+    assert(tv_equals(array->data[i], new_TValue_with_str(strs[i])));
+  }
 })
+
+TEST_CASE(test_eq, {
+  TValue *a = new_TValue_with_integer(1);
+  TValue *b = new_TValue_with_integer(1);
+  TValue *c = new_TValue_with_integer(2);
+  assert(tv_equals(a, b) == true);
+  assert(tv_equals(a, c) == false);
+
+  a = new_TValue_with_str("abc");
+  b = new_TValue_with_str("abc");
+  c = new_TValue_with_str("def");
+  assert(tv_equals(a, b) == true);
+  assert(tv_equals(a, c) == false);
+
+  a = new_TValue_with_bool(true);
+  b = new_TValue_with_bool(true);
+  c = new_TValue_with_bool(false);
+  assert(tv_equals(a, b) == true);
+  assert(tv_equals(a, c) == false);
+})
+
+TEST_CASE(test_cmp, {
+  TValue *a = new_TValue_with_integer(1);
+  TValue *b = new_TValue_with_integer(1);
+  TValue *c = new_TValue_with_integer(2);
+
+  assert(tv_cmp(a, b) == 0);
+  assert(tv_cmp(a, c) == -1);
+  assert(tv_cmp(c, a) == 1);
+
+  a = new_TValue_with_str("abc");
+  b = new_TValue_with_str("abc");
+  c = new_TValue_with_str("def");
+
+  assert(tv_cmp(a, b) == 0);
+  assert(tv_cmp(a, c) == -1);
+  assert(tv_cmp(c, a) == 1);
+});
 
 void value_test() {
   test_null();
@@ -42,8 +93,8 @@ void value_test() {
   test_str();
   test_bool();
   test_array();
+  test_eq();
+  test_cmp();
 
   printf("[value_test] All of tests are passed\n");
 }
-
-// TValue *new_TValue_with_func(VMFunction *func) {
