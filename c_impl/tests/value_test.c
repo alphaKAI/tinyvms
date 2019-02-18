c3984720ef91a877e7978ff91acda568133e2999
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2019-02-18 19:30:11 +0900

    add more test cases to value_test

--- c_impl/tests/value_test.c
+++ c_impl/tests/value_test.c
@@ -67,6 +67,26 @@ TEST_CASE(test_eq, {
   c = new_TValue_with_bool(false);
   assert(tv_equals(a, b) == true);
   assert(tv_equals(a, c) == false);
+
+  Vector *va = new_vec();
+  Vector *vb = new_vec();
+  Vector *vc = new_vec();
+  TValue *t;
+  TValue *t2;
+  for (int i = 0; i < 10; i++) {
+    t = new_TValue_with_integer(i);
+    t2 = new_TValue_with_integer(9 - i);
+    vec_push(va, t);
+    vec_push(vb, t);
+    vec_push(vc, t2);
+  }
+
+  TValue *ta = new_TValue_with_array(va);
+  TValue *tb = new_TValue_with_array(vb);
+  TValue *tc = new_TValue_with_array(vc);
+
+  assert(tv_equals(ta, tb) == true);
+  assert(tv_equals(ta, tc) == false);
 })
 
 TEST_CASE(test_cmp, {
@@ -87,6 +107,57 @@ TEST_CASE(test_cmp, {
   assert(tv_cmp(c, a) == 1);
 });
 
+TEST_CASE(test_cmps, {
+  TValue *a = new_TValue_with_integer(1);
+  TValue *b = new_TValue_with_integer(1);
+  TValue *c = new_TValue_with_integer(2);
+
+  assert(tv_lt(a, b) == false);
+  assert(tv_lt(a, c) == true);
+  assert(tv_lt(c, a) == false);
+  assert(tv_lte(a, b) == true);
+  assert(tv_lte(a, c) == true);
+  assert(tv_lte(c, a) == false);
+  assert(tv_gt(a, b) == false);
+  assert(tv_gt(a, c) == false);
+  assert(tv_gt(c, a) == true);
+  assert(tv_gte(a, b) == true);
+  assert(tv_gte(a, c) == false);
+  assert(tv_gte(c, a) == true);
+
+  a = new_TValue_with_str("abc");
+  b = new_TValue_with_str("abc");
+  c = new_TValue_with_str("def");
+
+  assert(tv_lt(a, b) == false);
+  assert(tv_lt(a, c) == true);
+  assert(tv_lt(c, a) == false);
+  assert(tv_lte(a, b) == true);
+  assert(tv_lte(a, c) == true);
+  assert(tv_lte(c, a) == false);
+  assert(tv_gt(a, b) == false);
+  assert(tv_gt(a, c) == false);
+  assert(tv_gt(c, a) == true);
+  assert(tv_gte(a, b) == true);
+  assert(tv_gte(a, c) == false);
+  assert(tv_gte(c, a) == true);
+})
+
+TEST_CASE(test_logics, {
+  TValue *tv_true = new_TValue_with_bool(true);
+  TValue *tv_false = new_TValue_with_bool(false);
+
+  assert(tv_and(tv_true, tv_true) == true);
+  assert(tv_and(tv_true, tv_false) == false);
+  assert(tv_and(tv_false, tv_true) == false);
+  assert(tv_and(tv_false, tv_false) == false);
+
+  assert(tv_or(tv_true, tv_true) == true);
+  assert(tv_or(tv_true, tv_false) == true);
+  assert(tv_or(tv_false, tv_true) == true);
+  assert(tv_or(tv_false, tv_false) == false);
+})
+
 void value_test() {
   test_null();
   test_integer();
@@ -95,6 +166,8 @@ void value_test() {
   test_array();
   test_eq();
   test_cmp();
+  test_cmps();
+  test_logics();
 
   printf("[value_test] All of tests are passed\n");
 }
